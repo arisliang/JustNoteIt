8179d1799b52ab958e695b1a74c7569ae97cee38
Aris Liang <liangyi2012@gmail.com>
2015-04-18 00:31:36 +0800

    wires up.

---
+++ JustNoteIt.Client.Desktop/AppBootstrapper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Ninject;
+using JustNoteIt.Client.Desktop.ViewModels;
+
+namespace JustNoteIt.Client.Desktop
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        #region fields and properties
+        private IKernel __Kernel = null;
+        private IKernel _Kernel
+        {
+            get
+            {
+                if (__Kernel == null)
+                {
+                    __Kernel = new StandardKernel();
+                }
+
+                return __Kernel;
+            }
+        }
+
+        #endregion
+
+        #region constructor
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        #endregion
+
+        #region overrides
+        protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+
+            DisplayRootViewFor<MainViewModel>();
+        }
+
+        protected override void Configure()
+        {
+            base.Configure();
+        }
+
+        protected override void OnExit(object sender, EventArgs e)
+        {
+            _Kernel.Dispose();
+
+            base.OnExit(sender, e);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return _Kernel.Get(service);
+            }
+            else
+            {
+                return _Kernel.Get(service, key);
+            }
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _Kernel.GetAll(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _Kernel.Inject(instance);
+        }
+
+        #endregion
+    }
+}
---
+++ JustNoteIt.Client.Desktop/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustNoteIt.Client.Desktop.ViewModels
+{
+    public class MainViewModel : PropertyChangedBase
+    {
+        public const string WindowTitleDefault = "So. Just Note It.";
+
+        private string _windowTitle = WindowTitleDefault;
+
+        public string WindowTitle
+        {
+            get
+            {
+                return _windowTitle;
+            }
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+    }
+}
